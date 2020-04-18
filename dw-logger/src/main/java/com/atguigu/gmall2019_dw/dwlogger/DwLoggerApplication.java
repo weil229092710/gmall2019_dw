349c73687b876eef0cf8e336fe126f2e05d510f6
wei229092710 <229092710@qq.com>
2020-04-18 19:51:58 +0800

    实时开发项目框架搭建

---
+++ dw-logger/src/main/java/com/atguigu/gmall2019_dw/dwlogger/DwLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall2019_dw.dwlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwLoggerApplication.class, args);
+    }
+
+}
