f6df9d49788e5efe587c08df94c75044a9a77379
wei229092710 <229092710@qq.com>
2020-04-18 20:18:41 +0800

    实时开发项目框架搭jjk

--- common/src/main/java/constant/GmallConstants.java
+++ common/src/main/java/constant/GmallConstants.java
@@ -7,7 +7,7 @@ public class GmallConstants {
     public static final String  KAFKA_TOPIC_ORDER="GMALL_ORDER";
     public static final String  KAFKA_TOPIC_ORDER_DETAIL="GMALL_ORDER_DETAIL";
     public static final String  KAFKA_TOPIC_USER_INFO="GMALL_USER_INFO";
-  //dfg
+  //dfgreg
     public static final String  ES_INDEX_ALERT="gmall_coupon_alert_0311";
     public static final String  ES_INDEX_SALE="gmall0311_sale_detail";
 
