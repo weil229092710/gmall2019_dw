92978dfcc0c3455beebc62d06404a54f62149b98
wei229092710 <229092710@qq.com>
2020-04-18 21:23:11 +0800

    sdfs电饭锅

--- common/src/main/java/constant/GmallConstants.java
+++ common/src/main/java/constant/GmallConstants.java
@@ -7,7 +7,7 @@ public class GmallConstants {
     public static final String  KAFKA_TOPIC_ORDER="GMALL_ORDER";
     public static final String  KAFKA_TOPIC_ORDER_DETAIL="GMALL_ORDER_DETAIL";
     public static final String  KAFKA_TOPIC_USER_INFO="GMALL_USER_INFO";
-  //drwegskl圣诞节福利fdg
+  //drwegskl圣诞节福利fdg史蒂夫
 
     public static final String  ES_INDEX_ALERT="gmall_coupon_alert_0311";
     public static final String  ES_INDEX_SALE="gmall0311_sale_detail";
